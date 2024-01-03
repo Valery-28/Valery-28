bb1b3ec532816a89c31330c01a8d96c9b7900f08
Petchenko-LapTop <valera9445@gmail.com>
2024-01-03 16:50:15 +0300

    F

--- src/gerular/Task4.java
+++ src/gerular/Task4.java
@@ -8,37 +8,29 @@ import java.util.Arrays;
 public class Task4 {
     public static void main(String[] args) {
         String word = "Поменяйте местами первое самое длинное слово c последним самым коротким";
-        String [] str = word.split(" ");
-        String [] str2 = new String[str.length];
-        int lengthStr;
-        int maxStr = 0;
-        int minStr = 999;
-        int indexMax=0;
-        int indexMin=0;
-        String strMin = "";
-        String strMax = "";
-        for(String words : str){
-            lengthStr = words.length();
-            if(lengthStr > maxStr){
-                maxStr = lengthStr;
-                strMax = words;
-                indexMax = words.indexOf(strMax);
+        String[] str = word.split(" ");
+        int maxWord = 0;
+        int minWord = 0;
+        String temp;
+        String strMax = str[0];
+        String strMin = str[0];
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].length() > strMax.length()) {
+                maxWord = i;
+                strMax = str[i];
             }
-            if(lengthStr <= minStr){
-                minStr = lengthStr;
-                strMin = words;
-                indexMin = words.indexOf(strMin);
+            if (str[i].length() <= strMin.length()) {
+                minWord = i;
+                strMin = str[i];
             }
-            str[indexMax]=strMin;
-            str[indexMin]=strMax;
-            System.arraycopy(str, 0, str2, 0, str.length);
-
         }
-
-
-        System.out.println("Самое большое слово в тексте: "+ strMax + ", его длина: " + maxStr );
-        System.out.println("Самое маленькое слово в тексте:  " + strMin + ", его длина: " + minStr);
-        System.out.println(Arrays.toString(str2));
-
+        System.out.println("До замены: " + Arrays.toString(str));
+        System.out.println("Max: " + strMax);
+        System.out.println("Min: " + strMin);
+        temp = str[minWord];
+        str[minWord] = str[maxWord];
+        str[maxWord] = temp;
+        System.out.print("После замены: ");
+        Arrays.stream(str).forEach(e -> System.out.print(e + " "));
     }
 }
